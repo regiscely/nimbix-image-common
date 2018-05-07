dfad376d54c4f1480221a867c4a74d8119b69429
jhegge <johnathan.hegge@nimbix.net>
2018-05-07 17:38:02 -0500

    re #48
    add xterm, missing by default now

--- nimbix_desktop/install-ubuntu-tiger.sh
+++ nimbix_desktop/install-ubuntu-tiger.sh
@@ -72,7 +72,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -y update
 apt-get -y install librtmp0 || apt-get -y install librtmp1
 apt-get -y install wget python python-gtk2 gnome-icon-theme \
-    humanity-icon-theme tango-icon-theme xfce4 xfce4-terminal \
+    humanity-icon-theme tango-icon-theme xfce4 xfce4-terminal xterm \
     fonts-freefont-ttf xfonts-base xfonts-100dpi xfonts-75dpi \
     xfonts-scalable xauth firefox ristretto mesa-utils init-system-helpers
 
